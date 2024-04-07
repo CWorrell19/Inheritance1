6a76eb284910dc192ad66a0d77cd941b973f9aa3
CWorrell19 <164057541+CWorrell19@users.noreply.github.com>
2024-04-06 23:39:05 -0700

    Delete Inheritance-main directory

--- Inheritance-main/Instructor.java
+++
@@ -1,8 +0,0 @@
-public class Instructor extends User {
-    double salary;
-
-    public Instructor(String name) {
-        super(name);
-        System.out.println("Called the Instructor Constructor");
-    }
-}
